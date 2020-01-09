63662e3cf82e8486907aa00a0e3c2d7d4290668c
danzabarr <danzabarr@hotmail.co.uk>
2020-01-09 15:31:23 +0000

    Comment targeting behaviour in Mob

--- TowerDefence/Assets/Scripts/Mob.cs
+++ TowerDefence/Assets/Scripts/Mob.cs
@@ -7,8 +7,8 @@ public class Mob : MonoBehaviour
 {
     private NavMeshAgent agent;
 
-    private Base nearestBase;
-    private Block nearestBlock;
+    private Base targetBase;
+    private Block targetBlock;
 
     private Rigidbody rb;
 
@@ -57,85 +57,136 @@ public class Mob : MonoBehaviour
         if (rb == null)
             rb = GetComponent<Rigidbody>();
 
+        //Advance the counter timing the interval between repathing.
         setDestinationCounter += Time.deltaTime;
 
+
+        //If the mob is below -5 (arbitrary value) then it is destroyed. Used to remove mobs that are knocked out of the game area. 
         if (transform.position.y < -5)
         {
             OnDeath();
             Destroy(gameObject);
             return;
         }
+
+        //Advance the counter timing the interval between attacks.
         attackCounter += Time.deltaTime;
 
-        if (nearestBlock)
+
+        //IF the mob has a target block
+        if (targetBlock)
         {
-            float squareDistance = Map.SquareDistance(nearestBlock.transform.position, transform.position);
+
+            //Find the square distance from the mob to the target block.
+            float squareDistance = Map.SquareDistance(targetBlock.transform.position, transform.position);
+
+            //Square the attack range
             float range = attackRange * attackRange;
 
+            //IF the block is within the attack range
             if (squareDistance < range)
             {
+                //Stop moving (disable the NavMeshAgent)
                 agent.enabled = false;
 
+                //If the attack counter is beyond the attack interval, i.e. if the mob is ready to attack
                 if (attackCounter >= attackInterval)
                 {
+                    //Reset the attack counter
                     attackCounter = 0;
-                    nearestBlock.AddDamage(attackDamage);
+
+                    //Add damage to the target block
+                    targetBlock.AddDamage(attackDamage);
+
+                    //Trigger an OnAttack call
                     OnAttack();
                 }
 
+                //FINALLY RETURN, i.e. don't execute the code below which finds targets
                 return;
             }
         }
 
-        else if (nearestBase)
+        //ELSE IF the mob has a target base
+        else if (targetBase)
         {
-            float squareDistance = Map.SquareDistance(nearestBase.transform.position, transform.position);
+            //Find the square distance from the mob to the target base.
+            float squareDistance = Map.SquareDistance(targetBase.transform.position, transform.position);
+
+            //Square the attack range
             float range = attackRange * attackRange;
 
+            //IF the base is within attack range
             if (squareDistance < range)
             {
+                //Stop moving
                 agent.enabled = false;
 
+                //If the attack counter is beyond the attack interval
                 if (attackCounter >= attackInterval)
                 {
+                    //Reset the attack counter
                     attackCounter = 0;
-                    nearestBase.AddDamage(attackDamage);
+
+                    //Add damage to the target base
+                    targetBase.AddDamage(attackDamage);
+
+                    //Trigger and OnAttack call
                     OnAttack();
                 }
 
+                //FINALLY RETURN
                 return;
             }
         }
 
-        float magnitude = rb.velocity.magnitude;
-        if (magnitude < minimumRigidbodyVelocity)
+
+        //Get the current linear velocity of the mob by getting the magnitude of the 3D velocity vector from the Rigidbody
+        float velocity = rb.velocity.magnitude;
+
+        //IF the mob is moving slower than a defined minimum
+        if (velocity < minimumRigidbodyVelocity)
         {
-            //rb.velocity = Vector3.zero;
+            //Stop moving
             agent.enabled = true;
 
+            //IF the counter is beyond a defined interval
             if (setDestinationCounter >= updatePathInterval)
             {
-                if (Map.Instance.NearestBlock(transform.position, blockAggroRange, out nearestBlock))
+                //Get new targets.
+
+                //First check for blocks.
+                //IF Map.NearestBlock(position, range, out block) returns true, a block was found within range of the position passed in. The NEAREST block it found is passed out via the targetBlock variable.
+                if (Map.Instance.NearestBlock(transform.position, blockAggroRange, out targetBlock))
                 {
-                    if (!agent.hasPath || Map.SquareDistance(nearestBlock.transform.position, agent.destination) > updatePathMinimumDistanceSquared)
+                    //IF the NavMeshAgent has no path/destination currently OR the distance between the CURRENT destination and the NEW destination is greater than a minimum
+                    //NB this check is done so that the NavMeshAgent doesn't do unnecessary repathing. It only repaths when either A. It doesn't have a path, or B. its current path is too far off from where it should be going.
+                    if (!agent.hasPath || Map.SquareDistance(targetBlock.transform.position, agent.destination) > updatePathMinimumDistanceSquared)
                     {
-                        agent.SetDestination(nearestBlock.transform.position);
+                        //Reset the set destination counter
                         setDestinationCounter = 0;
+                        
+                        //Set a new destination for the NavMeshAgent
+                        agent.SetDestination(targetBlock.transform.position);
                     }
                 }
-                else if (Map.Instance.NearestStructure(transform.position, out nearestBase))
+
+                //Otherwise if no block was found...
+                //IF Map.NearestStructure(position, out base) returns true, a Structure of type Base (because the out parameter targetBase is of type Base and the method uses type generics - I can explain this more if you want)
+                //a Base was found. The NEAREST base it found is passed out via the targetBase variable.
+                else if (Map.Instance.NearestStructure(transform.position, out targetBase))
                 {
-                    if (!agent.hasPath || Map.SquareDistance(nearestBase.transform.position, agent.destination) > updatePathMinimumDistanceSquared)
+                    //IF the NavMeshAgent has no path/destination currently OR the distance between the CURRENT destination and the NEW destination is greater than a minimum
+                    if (!agent.hasPath || Map.SquareDistance(targetBase.transform.position, agent.destination) > updatePathMinimumDistanceSquared)
                     {
-                        agent.SetDestination(nearestBase.transform.position);
+                        //Reset the set destination counter
                         setDestinationCounter = 0;
+
+                        //Set a new destination for the NavMeshAgent
+                        agent.SetDestination(targetBase.transform.position);
                     }
                 }
             }
-        }
-        else
-        {
-           // agent.enabled = false;
         }
     }
 
